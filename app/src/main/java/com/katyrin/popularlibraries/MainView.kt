86467ecd66360d9915cbf176f5b83e0c4f9b363a
Roman <katyrinroman@mail.ru>
2021-05-23 23:53:59 +0700

    Copy start project

---
+++ app/src/main/java/com/katyrin/popularlibraries/MainView.kt
@@ -0,0 +1,5 @@
+package com.katyrin.popularlibraries
+
+interface MainView {
+    fun setButtonText(index: Int, text: String)
+}
---
+++ app/src/main/java/com/katyrin/popularlibraries/presenter/MainPresenter.kt
@@ -0,0 +1,27 @@
+package com.katyrin.popularlibraries.presenter
+
+import com.katyrin.popularlibraries.MainView
+import com.katyrin.popularlibraries.R
+import com.katyrin.popularlibraries.model.CountersModel
+
+class MainPresenter(val view: MainView) {
+    val model = CountersModel()
+
+    //Архитектурная ошибка. В качестве практического задания -- исправить
+    fun counterClick(id: Int){
+        when(id){
+            R.id.btn_counter1 -> {
+                val nextValue = model.next(0)
+                view.setButtonText(0, nextValue.toString())
+            }
+            R.id.btn_counter2 -> {
+                val nextValue = model.next(1)
+                view.setButtonText(1, nextValue.toString())
+            }
+            R.id.btn_counter3 -> {
+                val nextValue = model.next(2)
+                view.setButtonText(2, nextValue.toString())
+            }
+        }
+    }
+}
